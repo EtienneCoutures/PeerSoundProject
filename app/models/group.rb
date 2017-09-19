2adecf33fa8a640b01750d7adb1cd343d3e64254
Etienne Coutures <etienne.coutures@epitech.eu>
2017-09-20 07:52:58 +0800

    RANGEMENT

--- app/models/group.rb
+++
@@ -1,6 +0,0 @@
-class Group < ApplicationRecord
-  
-  has_many :musics
-  has_and_belongs_to_many :users
-
-end
--- db/migrate/20170313140528_create_musics.rb
+++
@@ -1,9 +0,0 @@
-class CreateMusics < ActiveRecord::Migration[5.0]
-  def change
-    create_table :musics do |t|
-      t.references :group, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
