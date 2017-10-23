e248cc57b6f4aebfb744d08d0e1f3f20f42182eb
itiz <etienne.coutures@epitech.eu>
2017-10-24 05:53:54 +0700

    c'est pas encore ca

--- WebApp/public/controller/homeController.js
+++ WebApp/public/controller/homeController.js
@@ -1,10 +1,6 @@
 var pspApp = angular.module('PspHeader', [])
 	.controller('homeController', ['$scope', function($scope) {
-	$scope.template = {name: "home.html", url: "/../views/home.html"};
-}]);
-
-function headerController($scope) {
     $scope.header = {name: "header.html", url: "/../views/header.html"};
+		$scope.template = {name: "home.html", url: "/../views/home.html"};
 		$scope.footer = {name: "footer.html", url: "/../views/footer.html"};
-		$scope.message = 'MrBiscotte';
-}
+}]);
