efec9a62c7fa60ae892de968af2a196ebe237fb9
itiz <etienne.coutures@epitech.eu>
2017-11-14 14:04:44 +0700

    propre

--- WebApp/public/controller/footerController.js
+++
@@ -1,9 +0,0 @@
-var pspApp = angular.module('PspHeader', [])
-	.controller('footerController', ['$scope', function($scope) {
-	$scope.template = {name: "footer.html", url: "/../views/footer.html"};
-}]);
-
-function footerController($scope) {
-    $scope.header = {name: "footer.html", url: "/../views/footer.html"};
-		$scope.message = 'MrBiscotte';
-}
