52a91a36e1734baccc36f04bcb7842eef3d75895
Unknown <gabriel.miro@epitech.eu>
2018-05-22 16:18:07 +0900

    plus de possibilité d'ajout d'une mauvaise musique

--- PlugIn/src/js/takeMusic.js
+++ PlugIn/src/js/takeMusic.js
@@ -83,58 +83,97 @@ function sleep(miliseconds) {
     var currentTime = new Date().getTime();
     while (currentTime + miliseconds >= new Date().getTime()) {}
 }
+scApiUrl = function(url, apiKey) {
+    //https://api.soundcloud.com/resolve?url=https://soundcloud.com/rapqualite/vald-desaccorde-exclu-xeu&format=json&consumer_key=htuiRd1JP11Ww0X72T1C3g&callback=?
+    var useSandBox = false;
+        $doc = $(document);
+        var  domain = useSandBox ? 'sandbox-soundcloud.com' : 'soundcloud.com';
+    var  secureDocument = (document.location.protocol === 'https:');
+    var resolver = ( secureDocument || (/^https/i).test(url) ? 'https' : 'http') + '://api.' + domain + '/resolve?url=',
+        params = 'format=json&consumer_key=' + apiKey +'&callback=?';
+    // force the secure url in the secure environment
+    console.log(url);
+    if( secureDocument ) {
+      url = url.replace(/^http:/, 'https:');
+      console.log(url);
+    }
 
+    // check if it's already a resolved api url
+    if ( (/api\./).test(url) ) {
+      return url + '?' + params;
+    } else {
+      return resolver + url + '&' + params;
+    }
+  };
 function sendMusic() {
-    var title = document.getElementById("boxUrl");
-    if (title.value && artist.value && date.value && description.value) {
-        var infs = {
-            title  : title.value,
-            artist : artist.value,
-            date : date.value
-        }
-        textarea1.className = '';
-        textarea2.className = '';
-        popError.className = '';
-        sessionStorage.setItem("infos", JSON.stringify(infs));
-       var musicAdded = 1;
-       localStorage.setItem('musicAdded', musicAdded);
-       console.log("agagagagaggagag", JSON.parse(localStorage.getItem("id")) + '-' + artist.value + '-' + boxUrl.value + '-' + description.value + '-' + url);
-       jQuery.post('https://localhost:8000/api/music/',{
-            usr_id: JSON.parse(localStorage.getItem("id")),
-            music_source: "soundcloud",
-            music_group: artist.value,
-            music_name: boxUrl.value,
-            music_description: description.value,
-            music_url: url,
-            music_date: date.value
-       },
-       function (data){
-           if (data){
-               console.log(data);
-                idMusic = data.Music.music_id;
-                jQuery.post('https://localhost:8000/api/musiclink/',{
+    var biscotte = 0;
+    var testAPIURL = scApiUrl(url, 'nYw8DGbKym7Ph6LR1EaSxD8Dmj5rkCwa');
+    console.log("testAPIURL !!!!!" + testAPIURL);
+    $.getJSON(testAPIURL, function() {
+        console.log("success");
+        biscotte++;
+      })
+    setTimeout(function(){
+        if (biscotte == 0)
+            {
+            var musicAdded = 2;
+            localStorage.setItem('musicAdded', musicAdded);
+            document.location.href = "profil.html";
+            }
+        else
+            {
+                var title = document.getElementById("boxUrl");
+            if (title.value && artist.value && date.value && description.value) {
+                var infs = {
+                    title  : title.value,
+                    artist : artist.value,
+                    date : date.value
+                }
+                textarea1.className = '';
+                textarea2.className = '';
+                popError.className = '';
+                sessionStorage.setItem("infos", JSON.stringify(infs));
+               var musicAdded = 1;
+               localStorage.setItem('musicAdded', musicAdded);
+               console.log( JSON.parse(localStorage.getItem("id")) + '-' + artist.value + '-' + boxUrl.value + '-' + description.value + '-' + url);
+               jQuery.post('https://localhost:8000/api/music/',{
                     usr_id: JSON.parse(localStorage.getItem("id")),
-                    music_id: idMusic,
-                    playlist_id: Playlists.options[Playlists.selectedIndex].value
-                    },
-                    function (data){
-                        if (data){
-                           console.log(data);
-                    }
-                    });
-                    setTimeout(function(){
-                    document.location.href = "profil.html"
-                    }, 500);
-           }
-       }
-       );
-    }
-    else {
-        textarea1.className = 'blur';
-        textarea2.className = 'blur';
-        textarea3.className = 'blur';
-        popError.className = 'show';
-    }
+                    music_source: "soundcloud",
+                    music_group: artist.value,
+                    music_name: boxUrl.value,
+                    music_description: description.value,
+                    music_url: url,
+                    music_date: date.value
+               },
+               function (data){
+                   if (data){
+                       console.log(data);
+                        idMusic = data.Music.music_id;
+                        jQuery.post('https://localhost:8000/api/musiclink/',{
+                            usr_id: JSON.parse(localStorage.getItem("id")),
+                            music_id: idMusic,
+                            playlist_id: Playlists.options[Playlists.selectedIndex].value
+                            },
+                            function (data){
+                                if (data){
+                                   console.log(data);
+                            }
+                            });
+                            setTimeout(function(){
+                            document.location.href = "profil.html"
+                            }, 500);
+                   }
+               }
+               );
+            }
+            else {
+                textarea1.className = 'blur';
+                textarea2.className = 'blur';
+                textarea3.className = 'blur';
+                popError.className = 'show';
+            }
+            // callback to be executed when the AJAX request succeeds
+        }}, 1000);
 }
 
 function hidePop() {
