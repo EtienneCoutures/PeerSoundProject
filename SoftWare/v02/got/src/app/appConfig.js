124cf179c3c56e311428693d978129eeeb1ee4d8
itiz <etienne.coutures@epitech.eu>
2018-12-03 17:38:27 +0100

    commit

---
+++ SoftWare/v02/got/src/app/appConfig.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AppConfig = /** @class */ (function () {
+    function AppConfig() {
+        this.urlBase = "https://peersoundproject.com";
+    }
+    return AppConfig;
+}());
+exports.AppConfig = AppConfig;
+//# sourceMappingURL=appConfig.js.map
