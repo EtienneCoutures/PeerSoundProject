067597286eff4ed38721f3fed6bd2068292c260b
itiz <etienne.coutures@epitech.eu>
2018-12-03 17:31:21 +0100

    Add some front

--- SoftWare/v02/got/dist/out-tsc/src/app/music/musicLink.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MusicLink = /** @class */ (function () {
-    function MusicLink() {
-    }
-    return MusicLink;
-}());
-exports.MusicLink = MusicLink;
-//# sourceMappingURL=musicLink.js.map
